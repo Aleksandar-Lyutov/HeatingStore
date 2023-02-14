7dde9101b9b12f8b1ff85ccba49da57ccbc387d6
sasho.lyutov@gmail.com <sasho.lyutov@gmail.com>
2023-02-14 17:21:31 +0200

    first commit

---
+++ HeatingStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using HeatingStore.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeatingStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
